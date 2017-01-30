173891abce10759a93ce5c290f277972322406f2
Oumaima Belkadi <Oumaima@10.34.85.14>
2017-01-30 17:47:36 +0100

    Segunada version

--- src/Entorno/Entorno1.java
+++ src/Entorno/Entorno1.java
@@ -6,7 +6,7 @@ public class Entorno1 {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 		int num;
-		System.out.println("Introduzca numero: ");
+		System.out.println("Introduzca numeros: ");
 		num = teclado.nextInt();
 		System.out.println((num % 2) == 0 ? "PAR" : "IMPAR");
 
